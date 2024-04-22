d204220fbedfb216d9c267cba178222da22a8955
samueljuma <samueljuma1024@gmail.com>
2024-04-22 10:39:19 +0300

    implemnt column

--- My First Compose App/app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
+++ My First Compose App/app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.myfirstcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -31,24 +34,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface (color = MaterialTheme.colorScheme.primary) {
+        Column (
+            modifier = modifier.padding(12.dp)
+        ){
+            Text(
+                text = "Hello $name!",
+//                modifier = modifier.padding(24.dp)
+            )
+            Text(
+                text = "Hello $name!",
+//                modifier = modifier.padding(24.dp)
+            )
+        }
+
+    }
+
+}
+
+@Composable
+fun MyApp(modifier: Modifier =Modifier){
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting(name = "Android")
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyFirstComposeAppTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Greeting1Preview() {
-    MyFirstComposeAppTheme {
-        Greeting("Juma")
+        MyApp()
     }
 }
