430fb8c5550a1b2ce9aa0861e85753411e597221
samueljuma <samueljuma1024@gmail.com>
2024-04-23 11:44:23 +0300

    state hoisting in action

--- My First Compose App/app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
+++ My First Compose App/app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.myfirstcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +44,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(
+fun MyApp(modifier: Modifier =Modifier, ){
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        }else{
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun Greetings(
     modifier: Modifier =Modifier,
     names: List<String> = listOf("World", "Compose", "Juma")
 ){
@@ -48,11 +66,10 @@ fun MyApp(
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) } // both remember and rememeberSaveable work
+    val extraPadding = if(expanded) 48.dp else 0.dp
     Surface (
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -65,8 +82,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = "Hello")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { expanded.value =!expanded.value}) {
-                Text(if(expanded.value) "Show Less" else "Show More")
+            ElevatedButton(onClick = { expanded =!expanded}) {
+                Text(if(expanded) "Show Less" else "Show More")
                 
             }
         }
@@ -76,12 +93,46 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit
+){
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(
+            onClick = onContinueClicked,
+            modifier = Modifier.padding(vertical = 24.dp)
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
 
+@Preview
+@Composable
+fun MyAppPreview(){
+    MyFirstComposeAppTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     MyFirstComposeAppTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    MyFirstComposeAppTheme {
+        OnboardingScreen(onContinueClicked = {}) // Do Nothing | Perfect for Preview
     }
 }
