7cab3a13d6163e89ef4c6c6373767dcd244d4250
samueljuma <samueljuma1024@gmail.com>
2024-04-24 10:44:15 +0300

    handle big lists & state persistence

--- My First Compose App/app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
+++ My First Compose App/app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -7,8 +7,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -17,7 +18,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -45,7 +45,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier =Modifier, ){
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     Surface(modifier) {
         if (shouldShowOnboarding){
             OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
@@ -58,10 +58,10 @@ fun MyApp(modifier: Modifier =Modifier, ){
 @Composable
 fun Greetings(
     modifier: Modifier =Modifier,
-    names: List<String> = listOf("World", "Compose", "Juma")
+    names: List<String> = List(1000) {"$it"}
 ){
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items (items = names){ name ->
             Greeting(name = name)
         }
     }
@@ -123,7 +123,7 @@ fun MyAppPreview(){
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun GreetingPreview() {
+fun GreetingsPreview() {
     MyFirstComposeAppTheme {
         Greetings()
     }
